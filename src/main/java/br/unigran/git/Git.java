b9658b238ab4077ecd9da6bb6a689a4d96015403
andreunigran <andre.martins@unigran.br>
2024-04-23 20:07:07 -0400

    fdsfds

--- src/main/java/br/unigran/git/Git.java
+++ src/main/java/br/unigran/git/Git.java
@@ -11,6 +11,6 @@ package br.unigran.git;
 public class Git {
 
     public static void main(String[] args) {
-        System.out.println("Ola mundo");
+        System.out.println("Ola mundo.");
     }
 }
